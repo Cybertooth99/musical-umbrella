0b7476900d1e5eab36291a6a7eb7b800ce186565
Cybertooth99 <67939977+Cybertooth99@users.noreply.github.com>
2020-08-29 14:16:17 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+app = Flask(__name__)
+
+bot = ChatBot("Siri")
+trainer = ChatterBotCorpusTrainer(bot)
+trainer.train("chatterbot.corpus.english")
+
+
+@app.route("/")
+def home():
+    return render_template("webpage.html")
+
+
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    return str(bot.get_response(userText))
+
+
+if __name__ == "__main__":
+    app.run()
